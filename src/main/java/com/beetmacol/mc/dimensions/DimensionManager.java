6286de86d3f113a88e054886a91010f65f9a275a
BeetMacol <beetmacol@gmail.com>
2021-03-22 12:02:43 +0100

    Project structure

---
+++ src/main/java/com/beetmacol/mc/dimensions/DimensionManager.java
@@ -0,0 +1,12 @@
+package com.beetmacol.mc.dimensions;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DimensionManager {
+	public static final String MOD_ID = "dimension-manager";
+	public static final Logger LOGGER = LogManager.getLogger("Dimension Manager Mod");
+
+	public static void init() {
+	}
+}
